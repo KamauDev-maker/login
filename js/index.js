ba555fc491e69d2ee3032f8b193a3b18ca9932b1
KamauDev-maker <oscarnjenga@gmail.com>
2022-04-20 10:31:41 +0300

    adding changes

--- js/index.js
+++ js/index.js
@@ -1,22 +1,22 @@
 
   
-function setFormMessage(formElement, type, message) {
-    const messageElement = formElement.querySelector(".form-message");
+// function setFormMessage(formElement, type, message) {
+//     const messageElement = formElement.querySelector(".form-message");
 
-    messageElement.textContent = message;
-    messageElement.classList.remove("form-message-success", "form-message-error");
-    messageElement.classList.add(`form-message--${type}`);
-}
+//     messageElement.textContent = message;
+//     messageElement.classList.remove("form-message-success", "form-message-error");
+//     messageElement.classList.add(`form-message--${type}`);
+// }
 
-function setInputError(inputElement, message) {
-    inputElement.classList.add("form-input-error");
-    inputElement.parentElement.querySelector(".form-input-error-message").textContent = message;
-}
+// function setInputError(inputElement, message) {
+//     inputElement.classList.add("form-input-error");
+//     inputElement.parentElement.querySelector(".form-input-error-message").textContent = message;
+// }
 
-function clearInputError(inputElement) {
-    inputElement.classList.remove("form-input-error");
-    inputElement.parentElement.querySelector(".form-input-error-message").textContent = "";
-}
+// function clearInputError(inputElement) {
+//     inputElement.classList.remove("form-input-error");
+//     inputElement.parentElement.querySelector(".form-input-error-message").textContent = "";
+// }
 
 document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("#login");
